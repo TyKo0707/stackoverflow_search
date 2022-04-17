c7502f26c2965055623c759f1623540da2a02121
Kyryl Sidak <ksidak7@gmail.com>
2022-04-17 23:46:38 +0300

    Changed getting data

--- collect_data/getting_data.py
+++ collect_data/getting_data.py
@@ -1,8 +1,19 @@
 from google.cloud import bigquery
 import os
+from environs import Env
+import gdown
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:\\Users\\38097\Downloads\stackoverflow-347020-453c5a423922.json"
+env = Env()
+env.read_env()
+URL = env.str("URL")
+OUT_FILE = env.str("OUT_FILE")
+
+gdown.download(URL, OUT_FILE, quiet=False)
+out_file = os.path.abspath(OUT_FILE)
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = out_file
 client = bigquery.Client()
+
 query = """
     SELECT q.id, q.title, q.body, q.tags, a.body as answers, a.score
     FROM `bigquery-public-data.stackoverflow.posts_questions`
@@ -12,7 +23,7 @@ query = """
 
 dataframe = (
     client.query(query)
-    .result()
-    .to_dataframe()
+        .result()
+        .to_dataframe()
 )
 dataframe.to_csv("out.csv", index=False)
