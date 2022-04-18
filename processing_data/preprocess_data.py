aef58798b1616184f63bdb5bd68f35d05e0b5fbe
TyKo0707 <super.family.timut@gmail.com>
2022-04-18 11:23:29 +0300

    Preprocess data using pandas (extraction questions with the most popular tags)

--- processing_data/preprocess_data.py
+++ processing_data/preprocess_data.py
@@ -1,3 +1,4 @@
+import heapq
 import pandas as pd
 
 chunk = pd.read_csv('../collect_data/out.csv', chunksize=1000)
@@ -12,20 +13,31 @@ df = df.dropna(axis=0)
 # Check if dataframe contains duplicated values
 print(df.duplicated().any())
 
-combination = {
-    # Function 1
-    'answers': {
-        'combined_answers': lambda x: "\n".join(x)
-    },
-    # Function 2
-    'score': {
-        'combined_score': 'sum'
-    }
-}
-
 # Combining duplicate titles with one answer to one title with many answers (also combining their score)
 grouped_data = df.groupby(['id', 'title', 'body', 'tags'], as_index=False)\
     .agg(combined_answers=('answers', lambda x: "\n".join(x)), combined_score=('score', 'sum'))
 de_duplicated_data = pd.DataFrame(grouped_data)
 
 print(de_duplicated_data.head(), de_duplicated_data.columns)
+
+# Convert raw text data of tags into lists
+de_duplicated_data.tags = de_duplicated_data.tags.apply(lambda x: x.split('|'))
+
+# Make a dictionary to count the frequencies for all tags
+tag_freq_dict = {}
+for tags in de_duplicated_data.tags:
+    for tag in tags:
+        if tag not in tag_freq_dict:
+            tag_freq_dict[tag] = 0
+        else:
+            tag_freq_dict[tag] += 1
+
+most_common_tags = heapq.nlargest(100, tag_freq_dict, key=tag_freq_dict.get)
+
+final_indices = []
+for i, tags in enumerate(de_duplicated_data.tags.values.tolist()):
+    # The minimum length for common tags should be 2
+    if len(set(tags).intersection(set(most_common_tags))) > 1:
+        final_indices.append(i)
+
+final_data = de_duplicated_data.iloc[final_indices]
