fbb359b83facd3de6c36b89f5d84fc138d68d675
Kyryl Sidak <ksidak7@gmail.com>
2022-05-12 21:42:23 +0300

    Add search_pipeline.py function to the bot

--- bot_tg/handlers/users/search.py
+++ bot_tg/handlers/users/search.py
@@ -1,4 +1,5 @@
 from bot_tg.loader import dp
+from search_engine.prediction_model.search_pipeline import search_results
 from bot_tg.states.state_storage import States
 from aiogram.types import Message, ChatType
 from bot_tg.data.config import MAX_LIMIT
@@ -21,25 +22,28 @@ async def input_request(message: Message):
     TEXT = message.text
     await States.input_limit.set()
     await message.reply(fmt.text(f"Saving text..."
-                                 f"\nEnter the {fmt.hbold('limit')} of articles to be shown (less than {MAX_LIMIT}):"))
+                                 f"\nEnter the {fmt.hbold('limit')} of articles to be shown (\u2264 {MAX_LIMIT}):"))
 
 
 @dp.message_handler(state=States.input_limit)
 async def input_limit(message: Message):
     text = message.text
-    if text.isdecimal() and 0 < int(text) < MAX_LIMIT:
-        articles = "Articles:\n"
-        for i in range(int(text)):
-            articles += fmt.text(
-                fmt.text(f'{fmt.hbold("Title:")} {hlink(f"Article {i + 1}", "https://stackoverflow.com")}'),
-                fmt.text(f'\t\t\t{fmt.hbold("Similarity score:")} {round(random.uniform(1, 1.5), 5)}'),
-                fmt.text(f'\t\t\t{fmt.hbold("Tags:")} example | search | stackoverflow'),
-                fmt.text(f'\t\t\t{fmt.hbold("Body:")} {TEXT}\n'),
-                sep='\n'
-            )
-        await message.reply(articles, disable_web_page_preview=True)
-        await States.start.set()
+    global TEXT
+    if text.isdecimal() and 0 < int(text) <= MAX_LIMIT:
+        articles = search_results(TEXT, int(text))
+        print(articles)
+        # articles = "Articles:\n"
+        # for i in range(int(text)):
+        #     articles += fmt.text(
+        #         fmt.text(f'{fmt.hbold("Title:")} {hlink(f"Article {i + 1}", "https://stackoverflow.com")}'),
+        #         fmt.text(f'\t\t\t{fmt.hbold("Similarity score:")} {round(random.uniform(1, 1.5), 5)}'),
+        #         fmt.text(f'\t\t\t{fmt.hbold("Tags:")} example | search | stackoverflow'),
+        #         fmt.text(f'\t\t\t{fmt.hbold("Body:")} {TEXT}\n'),
+        #         sep='\n'
+        #     )
+        # await message.reply(articles, disable_web_page_preview=True)
+        # await States.start.set()
     else:
         await message.reply(fmt.text(f"{fmt.hbold('Incorrect input')}. "
-                                     f"Only non-negative integers are allowed which are less than {MAX_LIMIT}."
+                                     f"Only non-negative integers are allowed which are \u2264 {MAX_LIMIT}."
                                      f"\nTry again:"))
--- search_engine/prediction_model/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import tag_predictor
