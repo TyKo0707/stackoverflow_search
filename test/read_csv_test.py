f9ebf6f59fb5c9eb7efcd7e0f9d18cc4c6d8d330
Timur Khitrick <super.family.timut@gmail.com>
2022-05-13 15:38:54 +0300

    Create test directory and compare pd.read_csv() method with different engines (default and pyarrow)

---
+++ test/read_csv_test.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from environs import Env
+from timer import timer
+
+env = Env()
+env.read_env()
+FINAL_DATA = env.str("FINAL_DATA")
+
+with timer('open csv with 1 method'):
+    preprocessed_data1 = pd.read_csv(FINAL_DATA)
+with timer('open csv with 2 method'):
+    preprocessed_data2 = pd.read_csv(FINAL_DATA, engine="pyarrow")
+
+# [open csv with 1 method] done in 4 s
+# [open csv with 2 method] done in 1 s
---
+++ test/timer.py
@@ -0,0 +1,9 @@
+from contextlib import contextmanager
+import time
+
+
+@contextmanager
+def timer(name):
+    t0 = time.time()
+    yield
+    print(f'[{name}] done in {time.time() - t0:.0f} s')
