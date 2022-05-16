42e749b1877de1a5e1f546a374363d97369a58aa
Timur Khitrick <super.family.timut@gmail.com>
2022-05-16 03:35:19 +0300

    Getting data in new way (more data and more filters)

--- search_engine/collect_data/getting_data.py
+++ search_engine/collect_data/getting_data.py
@@ -18,17 +18,33 @@ out_file = os.path.abspath(OUT_FILE)
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = out_file
 client = bigquery.Client()
 
-query = """
-    SELECT q.id, q.title, q.body, q.tags, a.body as answers, a.score
-    FROM `bigquery-public-data.stackoverflow.posts_questions`
-    AS q INNER JOIN `bigquery-public-data.stackoverflow.posts_answers`
-    AS a ON q.id = a.parent_id LIMIT """ + DATE_SIZE
 
-try:
-    dataframe = (
-        client.query(query).result().to_dataframe()
-    )
-    dataframe.to_csv("out.csv", index=False)
-    logger.info(f"Data ({DATE_SIZE} items) was successfully downloaded and converted to CSV-file")
-except Exception as ex:
-    logger.exception("An error occurred while pulling data from the database")
+def build_query(min_id, max_id):
+    query = f"""
+        SELECT
+          q.id, q.title, q.body, q.tags, a.body as answers, a.score
+        FROM
+          `bigquery-public-data.stackoverflow.posts_questions` AS q
+        INNER JOIN
+          `bigquery-public-data.stackoverflow.posts_answers` AS a
+        ON
+          q.id = a.parent_id
+        WHERE
+          q.id BETWEEN {int(min_id)} AND {int(max_id)}
+          AND q.view_count > 250
+          AND q.accepted_answer_id IS NOT NULL
+        """
+    return query
+
+
+for i in range(6):
+    query = build_query(35e6 + 5e6 * i, 35e6 + 5e6 * (i + 1))
+    try:
+        dataframe = (
+            client.query(query).result().to_dataframe()
+        )
+        dataframe.to_parquet(f"out{i + 1}.csv", compression='gzip', index=False)
+        logger.info(f"Data (5m items (from {35 + 5 * i}m to {35 + 5 * (i + 1)}m)) "
+                    f"was successfully downloaded and converted to CSV-file")
+    except Exception as ex:
+        logger.exception("An error occurred while pulling data from the database")
