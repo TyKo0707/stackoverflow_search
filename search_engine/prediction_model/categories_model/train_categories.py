d6b88e7e1e8fd306639b458a357a3687b8b89c68
Timur Khitrick <super.family.timut@gmail.com>
2022-05-25 22:01:25 +0300

    Compare classifiers' results

--- search_engine/prediction_model/categories_model/train_categories.py
+++ search_engine/prediction_model/categories_model/train_categories.py
@@ -1,15 +1,22 @@
+from sklearn.dummy import DummyClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 from environs import Env
+from sklearn.linear_model import SGDClassifier, LogisticRegression, PassiveAggressiveClassifier, Perceptron
+from sklearn.metrics import hamming_loss
 from sklearn.model_selection import train_test_split
+import numpy as np
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import LinearSVC
 
 env = Env()
 env.read_env()
 
 DATA_PATH = env.str("DATA_PATH")
 
-df_title = pd.read_csv(DATA_PATH + 'dec_dataset.csv', engine='pyarrow')
-df_tags = pd.read_csv(DATA_PATH + 'enc_dataset.csv', engine='pyarrow')
+df_title = pd.read_csv(DATA_PATH + 'dec_dataset.csv', engine='pyarrow').sample(frac=1)
+df_tags = pd.read_csv(DATA_PATH + 'enc_dataset.csv', engine='pyarrow').sample(frac=1)[:2000]
 
 titles = df_title.title
 
@@ -42,3 +49,59 @@ X_train_title, X_test_title, y_train_title, y_test_title = train_test_split(titl
 
 X_train_tags, X_test_tags, y_train_tags, y_test_tags = train_test_split(tags, y_tags, test_size=0.2,
                                                                         random_state=0)
+
+
+def avg_jacard(y_true, y_pred):
+    jacard = np.minimum(y_true, y_pred).sum(axis=1) / np.maximum(y_true, y_pred).sum(axis=1)
+
+    return jacard.mean() * 100
+
+
+def print_score(y_pred, y_test, clf):
+    print("Clf: ", clf.__class__.__name__)
+    print(f"Jacard score: {avg_jacard(y_test, y_pred)}")
+    print(f"Hamming loss: {hamming_loss(y_pred, y_test) * 100}")
+    print("---")
+
+
+dummy = DummyClassifier()
+sgd = SGDClassifier()
+lr = LogisticRegression()
+mn = MultinomialNB()
+svc = LinearSVC()
+perceptron = Perceptron()
+pac = PassiveAggressiveClassifier()
+
+for classifier in [dummy, sgd, lr, mn, svc, perceptron, pac]:
+    clf = OneVsRestClassifier(classifier)
+    clf.fit(X_train_title, y_train_title)
+    y_pred = clf.predict(X_test_title)
+    print_score(y_pred, y_test_title, classifier)
+
+# Clf:  DummyClassifier
+# Jacard score: 0.0
+# Hamming loss: 3.0303030303030303
+# ---
+# Clf:  SGDClassifier
+# Jacard score: 46.9462324474752
+# Hamming loss: 1.8584287846922478
+# ---
+# Clf:  LogisticRegression
+# Jacard score: 46.73060990376649
+# Hamming loss: 1.858042537412877
+# ---
+# Clf:  MultinomialNB
+# Jacard score: 24.982474029698555
+# Hamming loss: 2.3796694882030422
+# ---
+# Clf:  LinearSVC
+# Jacard score: 50.09240966158944
+# Hamming loss: 1.7840761834133831
+# ---
+# Clf:  Perceptron
+# Jacard score: 46.54812737662779
+# Hamming loss: 2.841814357970116
+# ---
+# Clf:  PassiveAggressiveClassifier
+# Jacard score: 50.76689397319058
+# Hamming loss: 1.9534456154174464
