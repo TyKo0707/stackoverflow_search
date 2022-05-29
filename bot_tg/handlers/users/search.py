dd6bc1aa54137d8d2ebba5de675495f5a482aebb
Kyryl Sidak <ksidak7@gmail.com>
2022-05-30 00:29:22 +0300

    Modify search.py

--- bot_tg/handlers/users/search.py
+++ bot_tg/handlers/users/search.py
@@ -1,3 +1,4 @@
+from aiogram.dispatcher import FSMContext
 from bot_tg.loader import dp
 from search_engine.prediction_model.search_pipeline import search_results
 from bot_tg.states.state_storage import States
@@ -16,33 +17,40 @@ async def enter_search_mode(message: Message):
 
 
 @dp.message_handler(state=States.input_text)
-async def input_request(message: Message):
-    global TEXT
-    TEXT = message.text
+async def input_request(message: Message, state: FSMContext):
+    await state.update_data(search_text=message.text)
     await States.input_limit.set()
     await message.reply(fmt.text(f"Saving text..."
                                  f"\nEnter the {fmt.hbold('limit')} of articles to be shown (\u2264 {MAX_LIMIT}):"))
 
 
 @dp.message_handler(state=States.input_limit)
-async def input_limit(message: Message):
+async def input_limit(message: Message, state: FSMContext):
     text = message.text
-    global TEXT
     if text.isdecimal() and 0 < int(text) <= MAX_LIMIT:
+        await States.start.set()
         await message.reply("Searching...")
         num = int(text)
-        articles = search_results(TEXT, num)
-
-        result = "Articles:\n—————————"
-        for i in range(num):
-            result += fmt.text(
-                fmt.text(f'{fmt.hbold("Title:")} {hlink(articles[i]["title"].capitalize(), articles[i]["url"])}'),
-                fmt.text(f'{fmt.hbold("Similarity score:")} {articles[i]["similarity_score"]}'),
-                fmt.text(f'{fmt.hbold("Tags:")} {articles[i]["tags"]}'),
-                fmt.text(f'{fmt.hbold("Body:")} {articles[i]["body"][:75]}...\n—————————\n'),
-                sep='\n'
-            )
-        await message.reply(result, disable_web_page_preview=True)
+        user_data = await state.get_data()
+        search_text = user_data['search_text']
+        articles = search_results(search_text, num)
+        if len(articles) == 0:
+            await message.reply(f"No articles were found on request:\n{search_text}")
+        else:
+            result = ''
+            if len(articles) < num:
+                num = len(articles)
+                result += fmt.text(f"Only {fmt.hbold(str(num))} articles were found.\n")
+            result += "Articles:\n—————————"
+            for i in range(num):
+                result += fmt.text(
+                    fmt.text(f'{fmt.hbold("Title:")} {hlink(articles[i]["title"].capitalize(), articles[i]["url"])}'),
+                    fmt.text(f'{fmt.hbold("Similarity score:")} {articles[i]["similarity_score"]}'),
+                    fmt.text(f'{fmt.hbold("Tags:")} {articles[i]["tags"]}'),
+                    fmt.text(f'{fmt.hbold("Body:")} {articles[i]["body"][:75]}...\n—————————\n'),
+                    sep='\n'
+                )
+            await message.reply(result, disable_web_page_preview=True)
         await States.start.set()
     else:
         await message.reply(fmt.text(f"{fmt.hbold('Incorrect input')}. "
