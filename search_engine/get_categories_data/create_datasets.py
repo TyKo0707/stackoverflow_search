5c19325e5598a7b65ca0c0dc2a7b8e0b4aebdb53
Timur Khitrick <super.family.timut@gmail.com>
2022-05-30 23:55:01 +0300

    Change some features in get_categories_data 2

--- search_engine/get_categories_data/create_datasets.py
+++ search_engine/get_categories_data/create_datasets.py
@@ -1,6 +1,6 @@
 from data_from_site import CategoryDataset
 
-categories = ['c%23', 'c%2b%2b', 'javascript', 'python', 'java', 'php', 'android',
+categories = ['c%23', 'c%2b%2b', 'javascript', 'python', 'java', 'php', 'android', 'api'
               'jquery', 'iOS', 'database', 'r', 'c', 'asp.net', 'ruby', '.net', 'django', 'angularjs', 'reactjs',
               'regex', 'data-science', 'linux', 'spring', 'windows', 'git', 'macos', 'visual-studio', 'scala',
               'perl', 'rest', 'algorithm', 'excel', 'html', 'css']
